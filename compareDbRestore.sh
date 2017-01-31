e0ffdfc3a8dc1acc73ee7c0064ce11c0a582039d
鈴木 陽介 <suzuki.yosuke@moneyforward.co.jp>
2017-01-31 11:29:35 +0900

    exportを追加

--- compareDbRestore.sh
+++ compareDbRestore.sh
@@ -22,10 +22,11 @@ db_passfile="/opt/.keys/pdev_fdb.txt"
 
 s3BucketName="mysql-schema-info"
 s3passfile="/opt/.keys/s3.cfg"
-AWS_ACCESS_KEY_ID=`egrep access_key ${s3passfile} |awk '{print $3}'`
-AWS_SECRET_ACCESS_KEY=`egrep secret_key ${s3passfile} |awk '{print $3}'`
-echo ${AWS_ACCESS_KEY_ID}
-echo ${AWS_SECRET_ACCESS_KEY}
+
+export AWS_DEFAULT_REGION="ap-northeast-1"
+export AWS_ACCESS_KEY_ID=`egrep access_key ${s3passfile} |awk '{print $3}'`
+export AWS_SECRET_ACCESS_KEY=`egrep secret_key ${s3passfile} |awk '{print $3}'`
+
 
 { # output sh -
 if [ ${envid} -ne "prod" ] && [ ${envid} -ne "stg1" ] && [ ${envid} -ne "dev1" ] && [ ${envid} -ne "pdev" ]; then
