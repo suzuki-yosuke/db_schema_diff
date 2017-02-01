21c859639fd73c12991c2009000a627a4bc4a538
鈴木 陽介 <suzuki.yosuke@moneyforward.co.jp>
2017-02-01 10:05:58 +0900

    no message

--- dbBuild.sh
+++ dbBuild.sh
@@ -34,34 +34,28 @@ do
 
   mv ./config/database.yml ./config/database.yml.org
 
-  if [ ${ap_name} == "mf_internal"]; then
-    cat ../mysql_schema_diff/config/database.yml.mf_internal > ./config/database.yml
-  else
+  if [ ${ap_name} != "mf_internal"]; then
     sed -e "s/database: \(.*\)/database: ci_\1/g" ./config/database.yml.org > ./config/database.yml
   fi
 
+  bash ./ci.sh
 
-  if [ $? -ne 0 ] ; then
-     errorLog "Environment" "AP_NAME:${ap_name} database.ymlの変換に失敗しました。"
-     exit 1
-  fi
-
-  bundle install
-  if [ $? -ne 0 ] ; then
-     errorLog "Database" "AP_NAME:${ap_name} bundle installに失敗しました。"
-     exit 1
-  fi
-
-  rake db:create:all
-  if [ $? -ne 0 ] ; then
-     errorLog "Database" "AP_NAME:${ap_name} データベースの作成に失敗しました"
-     exit 1
-  fi
-  rake db:structure:load
-  if [ $? -ne 0 ] ; then
-     errorLog "Database" "AP_NAME:${ap_name} テーブルの作成に失敗しました"
-     exit 1
-  fi
-  infoLog "AP_CHECK" "AP_NAME:${ap_name} migrateを完了しました。"
+#  bundle install
+#  if [ $? -ne 0 ] ; then
+#     errorLog "Database" "AP_NAME:${ap_name} bundle installに失敗しました。"
+#     exit 1
+#  fi
+#
+#  rake db:create:all
+#  if [ $? -ne 0 ] ; then
+#     errorLog "Database" "AP_NAME:${ap_name} データベースの作成に失敗しました"
+#     exit 1
+#  fi
+#  rake db:structure:load
+#  if [ $? -ne 0 ] ; then
+#     errorLog "Database" "AP_NAME:${ap_name} テーブルの作成に失敗しました"
+#     exit 1
+#  fi
+#  infoLog "AP_CHECK" "AP_NAME:${ap_name} migrateを完了しました。"
 done
 } 2>&1 | tee -a ${log_file} ; exit ${PIPESTATUS[0]}
