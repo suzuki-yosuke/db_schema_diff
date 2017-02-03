c500cbffd236703bfb4f99a94f486aa077babeb9
鈴木 陽介 <suzuki.yosuke@moneyforward.co.jp>
2017-02-03 11:35:12 +0900

    MyNumberとCAのマイグレート方法を独自のものに変更

--- dbBuild.sh
+++ dbBuild.sh
@@ -39,6 +39,11 @@ rename_database_name() {
   mv ./config/database.yml ./config/database.yml.org
   sed -e "s/database: \(.*\)/database: ci_\1/g" ./config/database.yml.org > ./config/database.yml
 }
+rename_database_name_option_sql() {
+    mv ${option_sql} ${option_sql}.org
+    sed -e "s/CREATE DATABASE IF NOT EXISTS \(.*\)/CREATE DATABASE IF NOT EXISTS ci_\1/g" ${option_sql}.org > ${option_sql}
+}
+
 
 prepare_database_1() {
   bundle exec rake db:create:all db:structure:load
@@ -46,6 +51,9 @@ prepare_database_1() {
 }
 prepare_database_2() {
   bundle exec rake app:db:create:all app:db:migrate
+}
+prepare_database_3() {
+  bundle exec rake ridgepole:apply
 }
 
 { # output block
@@ -59,11 +67,21 @@ do
   rename_database_name
   if [ $ap_name == "mf_internal" ] || [ $ap_name == "pa_web" ];then
     prepare_database_2
+  elif [ $ap_name == "my_web" ];then
+    prepare_database_3
   elif [ $ap_name == "ca_web" ];then
     prepare_database_1
     targetdb="ci_ca_production"
-    option_sql="${WORKSPACE}/sys_deploy/lib/capistrano/sql/setup.img_ca_production.sql"
+
+    option_sql="${WORKSPACE}/ca_web/db/structure.sql"
+    echo "mysql -h ${targetDbHost} -u${targetDbID} -p${targetDbPass} ${targetdb} < ${option_sql}"
     mysql -h ${targetDbHost} -u${targetDbID} -p${targetDbPass} ${targetdb} < ${option_sql}
+
+    option_sql="${WORKSPACE}/sys_deploy/lib/capistrano/sql/setup.img_ca_production.sql"
+    rename_database_name_option_sql
+    echo "mysql -h ${targetDbHost} -u${targetDbID} -p${targetDbPass} ${targetdb} < ${option_sql}"
+    mysql -h ${targetDbHost} -u${targetDbID} -p${targetDbPass} ${targetdb} < ${option_sql}
+
   else
     prepare_database_1
   fi
