fed8a8581cb5447966a8adaab6974c0824490629
鈴木 陽介 <suzuki.yosuke@moneyforward.co.jp>
2017-02-07 11:05:54 +0900

    collate対応

--- compareDbRestore.sh
+++ compareDbRestore.sh
@@ -116,7 +116,7 @@ EOF
   sed -e 's/AUTO_INCREMENT=[0-9]*//g' ${G_YYYYMMDD}/${database}.sql |\
   sed -e 's/ROW_FORMAT=COMPRESSED//g'|\
   sed -e 's/KEY_BLOCK_SIZE=[0-9]*//g'|\
-  sed -e 's/character_set_client = utf8/character_set_client = utf8mb4 COLLATE utf8mb4_bin/g' > ${G_YYYYMMDD}/${database}_sed.sql
+  sed -e 's/character_set_client = utf8/character_set_client = utf8 COLLATE utf8mb4_bin/g' > ${G_YYYYMMDD}/${database}_sed.sql
 
   mysql -h ${targetDbHost} -u${targetDbID} -p${targetDbPass} ${envid}_${database} < ${G_YYYYMMDD}/${database}_sed.sql
 
