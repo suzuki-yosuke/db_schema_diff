4f0c69ffe60b1fa144d07a416441f88df32e9b90
鈴木 陽介 <suzuki.yosuke@moneyforward.co.jp>
2017-02-07 15:09:08 +0900

    --skip-table-optionsを付けてみる

--- dbCompare.sh
+++ dbCompare.sh
@@ -114,7 +114,7 @@ do
 
     echo "[Check DBName:ci_$dbName:${envid}_${dbName}(${dbHostName})]" > ${diffDb}
 #    /usr/local/bin/mysqldiff --force --difftype=sql \
-    /usr/local/bin/mysqldiff --force \
+    /usr/local/bin/mysqldiff --force --skip-table-options\
     --server1=${dbID}:${dbPass}@${dbHost} \
     --server2=${dbID}:${dbPass}@${dbHost} \
     ci_${dbName}:${envid}_${dbName} > ${diffDb}.tmp
