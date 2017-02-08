215928b99b7ae41f88de210a6b539d447c975f01
鈴木 陽介 <suzuki.yosuke@moneyforward.co.jp>
2017-02-08 14:40:37 +0900

    差分追加SQL
    Prod,stg,devにも追加するように修正

--- config/ci_img_mf_production.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `ci_img_mf_production`.`additional_request_media`;
-ALTER TABLE `ci_img_mf_production`.`transaction_file`;
---
+++ config/compare_img_mf_production.sql
@@ -0,0 +1,2 @@
+alter table envid_img_mf_production.additional_request_media row_format=compresse;
+alter table envid_img_mf_production.transaction_file row_format=compresse;
